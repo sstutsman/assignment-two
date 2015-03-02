7f5c81253cb89bd4305a4c9acc775d26e05791aa
jbenda43251 <bendajs@hotmail.com>
2015-03-02 17:44:20 -0600

    Added summation activity function

--- app/src/main/java/csis290/assignment2/MainActivity.java
+++ app/src/main/java/csis290/assignment2/MainActivity.java
@@ -1,15 +1,43 @@
 package csis290.assignment2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    EditText et1Input;
+    EditText et2Input;
+    EditText et3Input;
+    TextView tvReact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        et1Input = (EditText)findViewById(R.id.num_one);
+        et2Input = (EditText)findViewById(R.id.num_two);
+        et3Input = (EditText)findViewById(R.id.num_three);
+        tvReact = (TextView)findViewById(R.id.sum_button);
+
+        tvReact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Summing Numbers", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                intent.putExtra("key", et1Input.getText().toString());
+                intent.putExtra("key2", et2Input.getText().toString());
+                intent.putExtra("key3", et3Input.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/csis290/assignment2/SecondaryActivity.java
+++ app/src/main/java/csis290/assignment2/SecondaryActivity.java
@@ -1,17 +1,33 @@
 package csis290.assignment2;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class SecondaryActivity extends ActionBarActivity {
+public class SecondaryActivity extends Activity {
+
+    TextView tvResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+
+        tvResponse = (TextView) findViewById(R.id.tv_result);
+        tvResponse.append(getIntent().getStringExtra("key"));
+        tvResponse.append("+");
+        tvResponse.append(getIntent().getStringExtra("key2"));
+        tvResponse.append("+");
+        tvResponse.append(getIntent().getStringExtra("key3"));
+        tvResponse.append("=");
+        double sum = 0;
+        sum += Double.parseDouble(getIntent().getStringExtra("key"));
+        sum += Double.parseDouble(getIntent().getStringExtra("key2"));
+        sum += Double.parseDouble(getIntent().getStringExtra("key3"));
+        tvResponse.append(String.valueOf(sum));
     }
 
 
