3a78159467ef14a0ba553e88a672ced30f4c48c2
rosskyl <rosskyl@mnstate.edu>
2015-03-02 17:40:54 -0600

    first draft of assignment

--- app/src/main/java/csis290/assignment2/MainActivity.java
+++ app/src/main/java/csis290/assignment2/MainActivity.java
@@ -1,15 +1,58 @@
 package csis290.assignment2;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    private EditText etNum1;
+    private EditText etNum2;
+    private EditText etNum3;
+
+    private TextView tvButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        etNum1 = (EditText) findViewById(R.id.et_num1);
+        etNum2 = (EditText) findViewById(R.id.et_num2);
+        etNum3 = (EditText) findViewById(R.id.et_num3);
+
+        tvButton = (TextView) findViewById(R.id.tv_button);
+
+        tvButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    int num1 = new Integer(etNum1.getText().toString());
+                    int num2 = new Integer(etNum2.getText().toString());
+                    int num3 = new Integer(etNum3.getText().toString());
+
+                    int sum = num1 + num2 + num3;
+
+                    String sumText = sum + "";
+
+                    Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                    intent.putExtra("sum", sumText);
+
+                    startActivity(intent);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(MainActivity.this, "Enter numbers not letters", Toast.LENGTH_LONG).show();
+                }//end catch
+            }//end inner method onClick
+        });//end tvButton.setOnClickListener
         
-    }
-}
+    }//end method onCreate
+}//end class MainActivity
--- app/src/main/java/csis290/assignment2/SecondaryActivity.java
+++ app/src/main/java/csis290/assignment2/SecondaryActivity.java
@@ -4,22 +4,29 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class SecondaryActivity extends ActionBarActivity {
+
+    TextView tvSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
-    }
+
+        tvSum = (TextView) findViewById(R.id.tv_sum);
+
+        tvSum.setText(getIntent().getStringExtra("sum"));
+    }//end method onCreate
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_secondary, menu);
         return true;
-    }
+    }//end method onCreateOptionsMenu
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -27,8 +34,8 @@ public class SecondaryActivity extends ActionBarActivity {
 
         if (id == R.id.action_settings) {
             return true;
-        }
+        }//end if
 
         return super.onOptionsItemSelected(item);
-    }
-}
+    }//end method onOptionsItemSelected
+}//end class SecondaryActivity
