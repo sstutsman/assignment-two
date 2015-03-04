34dee70e556e1adf3051e1a9d618d99ed0027d00
Julio Salamanca <salamancju@mnstate.edu>
2015-03-04 15:53:49 -0600

    made the app work as it should

--- app/src/main/java/csis290/assignment2/SecondaryActivity.java
+++ app/src/main/java/csis290/assignment2/SecondaryActivity.java
@@ -1,17 +1,34 @@
 package csis290.assignment2;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class SecondaryActivity extends ActionBarActivity {
 
+    TextView answerLabel;
+    Button tryAgainButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+        Intent mainActivityIntent = getIntent();
+        tryAgainButton = (Button) findViewById(R.id.buttonTryAgain);
+        answerLabel = (TextView) findViewById(R.id.answerTextView);
+        answerLabel.setText(mainActivityIntent.getStringExtra("sum"));
+
+        tryAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 
