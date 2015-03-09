f585f7ac81d9eebb1646d4e5eaae4ab46e850f41
Thurind <d-maddux@hotmail.com>
2015-03-08 19:13:04 -0500

    made necessary changes to the activities

--- app/src/main/java/csis290/assignment2/MainActivity.java
+++ app/src/main/java/csis290/assignment2/MainActivity.java
@@ -1,15 +1,43 @@
 package csis290.assignment2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
+
+    EditText first;
+    EditText second;
+    EditText third;
+    int sum = 0;
+    String SUM = "";
+    TextView find;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        first = (EditText) findViewById(R.id.first);
+        second = (EditText) findViewById(R.id.second);
+        third = (EditText) findViewById(R.id.third);
+        find =(TextView) findViewById(R.id.find);
+
+        find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sum = Integer.parseInt(first.getText().toString()) + Integer.parseInt(second.getText().toString())
+                      + Integer.parseInt(third.getText().toString());
+                Intent intent = new Intent(MainActivity.this,SecondaryActivity.class);
+                intent.putExtra(SUM,sum);
+                startActivity(intent);
+            }
+
+        });
         
     }
 }
--- app/src/main/java/csis290/assignment2/SecondaryActivity.java
+++ app/src/main/java/csis290/assignment2/SecondaryActivity.java
@@ -1,17 +1,29 @@
 package csis290.assignment2;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class SecondaryActivity extends ActionBarActivity {
+
+    String SUM = "";
+    int sum = 0;
+    TextView result;
+    TextView intro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
+        Intent intent = getIntent();
+        sum = intent.getIntExtra(SUM, 0);
+        result = (TextView) findViewById(R.id.result);
+        intro = (TextView) findViewById(R.id.intro);
+        result.setText(String.valueOf(sum));
     }
 
 
