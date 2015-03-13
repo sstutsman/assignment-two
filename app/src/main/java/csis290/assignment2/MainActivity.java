313a88bf1658cee781f79dde7a5e39534e6f0e18
unknown <oa5579pw@BR161-27.mnstate.edu>
2015-03-12 19:10:37 -0500

    this is my final work

--- app/src/main/java/csis290/assignment2/MainActivity.java
+++ app/src/main/java/csis290/assignment2/MainActivity.java
@@ -2,14 +2,49 @@ package csis290.assignment2;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    int number1;
+    int number2;
+    int number3;
+
+    private EditText num1;
+    private EditText num2;
+    private EditText num3;
+    private TextView findSum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
+        num1 = (EditText) findViewById(R.id.num1Area);
+        num2 = (EditText) findViewById(R.id.num2Area);
+        num3 = (EditText) findViewById(R.id.num3Area);
+
+        findSum = (TextView) findViewById(R.id.findSumButton);
+
+
+        findSum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                number1 = Integer.parseInt(num1.getText().toString());
+                number2 = Integer.parseInt(num2.getText().toString());
+                number3 = Integer.parseInt(num3.getText().toString());
+
+                int sum = number1 + number2 + number3;
+
+                Intent intent = new Intent(MainActivity.this, SecondaryActivity.class);
+                intent.putExtra("solution", sum);
+                startActivity(intent);
+            }
+    });
+
     }
 }
--- app/src/main/java/csis290/assignment2/SecondaryActivity.java
+++ app/src/main/java/csis290/assignment2/SecondaryActivity.java
@@ -2,33 +2,21 @@ package csis290.assignment2;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class SecondaryActivity extends ActionBarActivity {
+
+    private TextView resultArea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
-    }
 
+        resultArea = (TextView) findViewById(R.id.sumArea);
+        int solution = getIntent().getIntExtra("solution", -1);
+        resultArea.setText(" " + solution);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_secondary, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
